f4aa9934e055fc693fb7012b144fd8ab487f1bff
deepaksahu3698 <deepaksahu3698@gmail.com>
2022-06-07 20:30:21 +0530

    React_ass1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+      <li>Andriod</li>
+      <li>Blackberry</li>
+      <li>iphone</li>
+      <li>Windows phone</li>
+      </ul>
+       <h1>Mobile Manufacturers</h1>
+     <ul type="square">
+     <li>Samsung</li>
+     <li>HTC</li>
+     <li>Micromax</li>
+     <li>Apple</li>
+     </ul>
+    </div>
+
+     
+    
+   
+  );
+}
+
+export default App;
